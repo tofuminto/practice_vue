e44f273b07781f247d305c7993219fabac7c803a
tofuminto <tototofumifumin@gmail.com>
2022-06-12 16:43:55 +0900

    add_filter

--- lesson-vue/department.js
+++ lesson-vue/department.js
@@ -13,9 +13,55 @@ Add Department
 <thead>
 	<tr>
 		<th>
+			<div class="d-flex flex-row">
+
+			<input class="form-control m-2"
+				v-model="DepartmentIdFilter"
+				v-on:keyup="FilterFn()"
+				placeholder="Filter">
+
+				<button type="button" class="btn btn-light"
+				@click="sortResult('DepartmentId',true)">
+				<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-down-square-fill" viewBox="0 0 16 16">
+				<path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm6.5 4.5v5.793l2.146-2.147a.5.5 0 0 1 .708.708l-3 3a.5.5 0 0 1-.708 0l-3-3a.5.5 0 1 1 .708-.708L7.5 10.293V4.5a.5.5 0 0 1 1 0z"/>
+				</svg>
+				</button>
+
+				<button type="button" class="btn btn-light"
+				@click="sortResult('DepartmentId',false)">
+				<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-up-square-fill" viewBox="0 0 16 16">
+				<path d="M2 16a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2zm6.5-4.5V5.707l2.146 2.147a.5.5 0 0 0 .708-.708l-3-3a.5.5 0 0 0-.708 0l-3 3a.5.5 0 1 0 .708.708L7.5 5.707V11.5a.5.5 0 0 0 1 0z"/>
+				</svg>
+				</button>
+
+			</div>
 			DepartmentId
 		</th>
 		<th>
+
+			<div class="d-flex flex-row">
+
+			<input class="form-control m-2"
+				v-model="DepartmentNameFilter"
+				v-on:keyup="FilterFn()"
+				placeholder="Filter">
+
+				<button type="button" class="btn btn-light"
+				@click="sortResult('DepartmentName',true)">
+				<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-down-square-fill" viewBox="0 0 16 16">
+				<path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm6.5 4.5v5.793l2.146-2.147a.5.5 0 0 1 .708.708l-3 3a.5.5 0 0 1-.708 0l-3-3a.5.5 0 1 1 .708-.708L7.5 10.293V4.5a.5.5 0 0 1 1 0z"/>
+				</svg>
+				</button>
+
+				<button type="button" class="btn btn-light"
+				@click="sortResult('DepartmentName',false)">
+				<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-up-square-fill" viewBox="0 0 16 16">
+				<path d="M2 16a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2zm6.5-4.5V5.707l2.146 2.147a.5.5 0 0 0 .708-.708l-3-3a.5.5 0 0 0-.708 0l-3 3a.5.5 0 1 0 .708.708L7.5 5.707V11.5a.5.5 0 0 0 1 0z"/>
+				</svg>
+				</button>
+
+			</div>
+
 			DepartmentName
 		</th>
 		<th>
@@ -91,7 +137,10 @@ data(){
 		departments:[],
 		modalTitle:"",
 		DepartmentName:"",
-		DepartmentId:0
+		DepartmentId:0,
+		DepartmentNameFilter:"",
+		DepartmentIdFilter:"",
+		departmentsWithoutFilter:[]
 	}
 },
 methods:{
@@ -99,6 +148,7 @@ methods:{
 		axios.get(variables.API_URL+"department")
 		.then(function (response){
 			this.departments=response.data;
+			this.departmentsWithoutFilter=response.data;
 		});
 	},
 	addClick(){
@@ -139,6 +189,30 @@ methods:{
 			this.refreshData();
 			alert(response.data);
 		});
+	},
+	FilterFn(){
+		var DepartmentIdFilter=this.DepartmentIdFilter;
+		var DepartmentNameFilter=this.DepartmentNameFilter;
+
+		this.departments=this.departmentsWithoutFilter.filter(
+			function(el){
+				return el.DepartmentId.toString().toLowerCase().includes(
+					DepartmentIdFilter.toString().trim().toLowerCase()
+				)&&
+				el.DepartmentName.toString().toLowerCase().includes(
+					DepartmentNameFilter.toString().trim().toLowerCase()
+				)
+			});
+	},
+	sortResult(prop, asc){
+		this.departments=this.departmentsWithoutFilter.sort(function(a,b){
+			if(asc){
+				return (a[prop]>b[prop])?1:((a[prop]<b[prop])?-1:0);
+			}
+			else{
+				return (b[prop]>a[prop])?1:((b[prop]<a[prop])?-1:0);
+			}
+		})
 	}
 },
 mounted:function(){
